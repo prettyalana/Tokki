aeb23ee1f2c639430a2642d10c95a7ff2007408a
Alana <129349619+prettyalana@users.noreply.github.com>
2025-10-20 20:58:00 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from unihiker_k10 import screen, rgb
+import time
+
+screen.init(dir=2)
+
+while True:
+	
+	# RGB LIGHT SHOW
+	rgb.write(num=0, R=255, G=192, B=203)  # Pink
+	rgb.write(num=1, R=255, G=182, B=193)  # Light pink  
+	rgb.write(num=2, R=255, G=105, B=180)  # Hot pink
+	
+	screen.draw_text(text="Pretty Alana",line=0)
+	screen.show_draw()
