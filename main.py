9d6ef5ca5e6ee54ac4441cd88c53a4955e999558
Alana <129349619+prettyalana@users.noreply.github.com>
2025-10-20 21:12:50 -0500

    Remove while loop to reduce memory usage and add comments

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from unihiker_k10 import screen, rgb
 import time
 
+# Initialize
 screen.init(dir=2)
 
-while True:
-	
-	# RGB LIGHT SHOW
-	rgb.write(num=0, R=255, G=192, B=203)  # Pink
-	rgb.write(num=1, R=255, G=182, B=193)  # Light pink  
-	rgb.write(num=2, R=255, G=105, B=180)  # Hot pink
-	
-	screen.draw_text(text="Alana",line=0)
-	screen.show_draw()
+# Show Tokki is alive!
+screen.draw_text(text="Alana", line=0)
+screen.show_draw()
+
+# Pretty pink lights!
+rgb.write(num=0, R=255, G=192, B=203)  # Pink
+rgb.write(num=1, R=255, G=182, B=193)  # Light pink  
+rgb.write(num=2, R=255, G=105, B=180)  # Hot pink
